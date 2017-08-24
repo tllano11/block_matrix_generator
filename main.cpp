821fc3b92dbf8a27ba0162dd86a59a5b2283d8a8
Tomas Felipe Llano Rios <tllano11@gmail.com>
2017-08-23 22:13:06 -0500

    Adding arguments parser and performing basic calculations.

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<getopt.h>
+#include<mpi.h>
+#include<math.h>
+#include<sys/sysinfo.h>
+
+#define ERROR 1;
+#define SUCCESS 0;
+
+using namespace std;
+
+int main (int argc, char** argv) {
+  int opt, num_procs, rank;
+  int* arguments;
+  struct sysinfo mem_info;
+  long matrix_size;
+  double mem_percentage;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  while ((opt = getopt(argc, argv, "p:s:h")) != EOF) {
+    switch (opt) {
+    case 'p':
+      mem_percentage = stof(optarg);
+      break;
+    case 's':
+      matrix_size = stol(optarg);
+      break;
+    case 'h':
+      cout << "\n\nUsage:\n"
+	   << "\r\t-p <Percentage of RAM to use>\n"
+	   << "\r\t-s <Matrix (NxN) size>\n"
+	   << endl;
+      break;
+    case '?':
+      cerr << "Use option -h to display a help message.";
+    default:
+      abort();
+    }
+  }
+
+
+  sysinfo(&mem_info);
+  double mem_to_use = mem_info.freeram * mem_percentage;
+  long row_size = matrix_size * sizeof(long double);
+  int total_rows = floor(mem_to_use /row_size);
+  int rows_per_proc = floor(total_rows / num_procs);
+
+  if (matrix_size < rows_per_proc) {
+    cerr << "This program is intendend to produce large matrices, "
+  	 << "which can not be loaded into RAM without swapping. "
+  	 << "Therefore a matrix of size " << matrix_size
+  	 << "is not suitable to produce given your system "
+  	 << "resources.";
+    abort();
+
+  } else if (rows_per_proc < 1) {
+    cerr << "Not even a single row of a " << matrix_size << "x"
+  	 << matrix_size << " matrix can be loaded without into"
+  	 << "your system RAM without swapping.";
+    abort();
+  }
+
+  //cout << (matrix_size / (long) total_rows) << endl;
+
+  //long double* data = new long double[rows_per_proc * matrix_size];
+  //delete data;
+
+
+  MPI_Finalize();
+  return SUCCESS;
+}
