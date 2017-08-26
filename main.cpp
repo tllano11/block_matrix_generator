a4c4edf417de207b6c90bdc03ced53fd3b2a05d7
Tomas Felipe Llano Rios <tllano11@gmail.com>
2017-08-26 09:26:24 -0500

    Generating random rows.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<random>
+#include<limits>
 #include<stdio.h>
 #include<ctype.h>
 #include<stdlib.h>
@@ -9,89 +11,126 @@
 
 #define ERROR 1
 #define SUCCESS 0
+#define MASTER 0
 
 using namespace std;
 
+void fill_with_random(long double* data, long data_size) {
+  random_device rd;
+  mt19937_64 mt(rd());
+
+  long double min = numeric_limits<long double>::min();
+  long double max = numeric_limits<long double>::max();
+  uniform_real_distribution<long double> urd(min, max);
+
+  for (long i = 0; i < data_size; ++i) {
+    data[i] = urd(mt);
+  }
+}
+
+void print_data(long double* data, long data_size) {
+  for (long i = 0; i < data_size; ++i) {
+    cout << data[i] << " ";
+  }
+  cout << endl;
+}
+
 int main (int argc, char** argv) {
-  int opt, num_procs, rank;
+  int opt, num_procs, rank, total_rows, rows_per_proc;
   struct sysinfo mem_info;
-  long matrix_size;
+  long matrix_size, row_size;
   double mem_percentage;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  while ((opt = getopt(argc, argv, "p:s:h")) != EOF) {
-    switch (opt) {
-    case 'p':
-      mem_percentage = stof(optarg);
-      break;
-    case 's':
-      matrix_size = stol(optarg);
-      break;
-    case 'h':
-      cout << "\n\nUsage:\n"
-	   << "\r\t-p <Percentage of RAM to use>\n"
-	   << "\r\t-s <Matrix (NxN) size>\n"
-	   << endl;
-      break;
-    case '?':
-      cerr << "Use option -h to display a help message.";
-    default:
-      MPI_Abort(MPI_COMM_WORLD, ERROR);
+  if (rank == MASTER) {
+    while ((opt = getopt(argc, argv, "p:s:h")) != EOF) {
+      switch (opt) {
+      case 'p':
+	mem_percentage = stof(optarg);
+	break;
+      case 's':
+	matrix_size = stol(optarg);
+	break;
+      case 'h':
+	cout << "\n\nUsage:\n"
+	     << "\r\t-p <Percentage of RAM to use>\n"
+	     << "\r\t-s <Matrix (NxN) size>\n"
+	     << endl;
+	break;
+      case '?':
+	cerr << "Use option -h to display a help message.";
+      default:
+	MPI_Abort(MPI_COMM_WORLD, ERROR);
+      }
     }
-  }
 
-  sysinfo(&mem_info);
-  double mem_to_use = mem_info.freeram * mem_percentage;
-  long row_size = matrix_size * sizeof(long double);
-  int total_rows = floor(mem_to_use /row_size);
-  int rows_per_proc = floor(total_rows / num_procs);
+    sysinfo(&mem_info);
+    double mem_to_use = mem_info.freeram * mem_percentage;
+    row_size = matrix_size * sizeof(long double);
+    total_rows = floor(mem_to_use /row_size);
+    rows_per_proc = floor(total_rows / num_procs);
 
 #ifdef DEBUG
-  if (rank == 0) {
     cout << string(50, '*') << endl;
     cout << "Total RAM to use = " << mem_to_use << endl;
     cout << "Single row size in RAM = " << row_size << endl;
     cout << "Rows to produce per iteration = " << total_rows << endl;
     cout << "Rows to produce per processor = " << rows_per_proc << endl;
     cout << string(50, '*') << endl;
-  }
 #endif // DEBUG
 
-  if (matrix_size < rows_per_proc) {
-    cerr << "This program is intendend to produce large matrices, "
-  	 << "which can not be loaded into RAM without swapping. "
-  	 << "Therefore a matrix of size " << matrix_size
-  	 << "is not suitable to produce given your system "
-  	 << "resources.";
-    abort();
+    if (matrix_size < rows_per_proc) {
+      cerr << "This program is intendend to produce large matrices, "
+	   << "which can not be loaded into RAM without swapping. "
+	   << "Therefore a matrix of size " << matrix_size
+	   << " is not suitable to produce given your system "
+	   << "resources.";
+      MPI_Abort(MPI_COMM_WORLD, ERROR);
 
-  } else if (rows_per_proc < 1) {
-    cerr << "Not even a single row of a " << matrix_size << "x"
-  	 << matrix_size << " matrix can be loaded without into"
-  	 << "your system RAM without swapping.";
-    abort();
+    } else if (rows_per_proc < 1) {
+      cerr << "Not even a single row of a " << matrix_size << "x"
+	   << matrix_size << " matrix can be loaded into "
+	   << "your system RAM without swapping.";
+      MPI_Abort(MPI_COMM_WORLD, ERROR);
+    }
   }
 
-  //cout << (matrix_size / (long) total_rows) << endl;
+  MPI_Bcast(&row_size, 1, MPI_LONG, MASTER, MPI_COMM_WORLD);
+  MPI_Bcast(&matrix_size, 1, MPI_LONG, MASTER, MPI_COMM_WORLD);
+  MPI_Bcast(&total_rows, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+  MPI_Bcast(&rows_per_proc, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
 
-  //long double* data = new long double[rows_per_proc * matrix_size];
-  //delete data;
-
-  int initial_it_row = rank * rows_per_proc;
-  while (initial_it_row < matrix_size) {
 #ifdef DEBUG
-      cout << string(50, '*') << endl;
-      cout << "First row for processor " << rank
-	   << " = " << initial_it_row << endl;
-      cout << "Final row for processor " << rank
-	   << " = " << initial_it_row + rows_per_proc - 1 << endl;
-      cout << string(50, '*') << endl;
+  cout << "Rank " << rank << " received row_size = "
+       << row_size << endl;
+  cout << "Rank " << rank << " received total_rows = "
+       << total_rows << endl;
+  cout << "Rank " << rank << " received rows_per_proc = "
+       << rows_per_proc << endl;
+  cout << "Rank " << rank << " received matrix_size = "
+       << matrix_size << endl;
 #endif //DEBUG
 
-      initial_it_row += total_rows;
+  long initial_it_row = rank * rows_per_proc;
+  long final_it_row;
+  long data_size = rows_per_proc * matrix_size;
+  long double* data = new long double[data_size];
+
+  while (initial_it_row < matrix_size) {
+    final_it_row = initial_it_row + rows_per_proc - 1;
+    if (final_it_row > matrix_size) {
+      delete data;
+      final_it_row = matrix_size;
+      data_size = matrix_size * (final_it_row - initial_it_row);
+      data = new long double[data_size];
+    }
+    fill_with_random(data, data_size);
+    cout << "Rank " << rank << " produced matrix: " << endl;
+    print_data(data, data_size);
+    initial_it_row += total_rows;
   }
 
   MPI_Finalize();
