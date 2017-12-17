58c117bbcf2c901850515dddbd6c78bab08e77e6
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-12-17 10:21:13 -0500

    Fixing matrix - vector multiplication

--- gpu_mul2.cu
+++ gpu_mul2.cu
@@ -5,7 +5,7 @@ __global__ void multiply(double* m, double* v, double* r,
 		       uint32_t rows, uint32_t cols) {
   uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < rows) {
-    uint32_t begin = idx*rows;
+    uint32_t begin = idx*cols;
     uint32_t end = begin + cols;
     uint32_t j = 0;
 
@@ -16,11 +16,11 @@ __global__ void multiply(double* m, double* v, double* r,
 }
 
 int main() {
-  double a[4] = {1, 1, 1, 1};
-  double x[2] = {1, 1};
-  double b[2] = {0, 0};
+  double a[6] = {1,-1,2,0,-3,1};
+  double x[3] = {2,1,0};
+  double b[2] = {0,0};
   uint32_t rows = 2;
-  uint32_t cols = 2;
+  uint32_t cols = 3;
 
   double *a_g, *x_g, *b_g;
 
@@ -34,7 +34,7 @@ int main() {
 
   int tpb = 32;
   int bpg = rows*cols + (tpb - 1) / tpb;
-  multiply <<< bpg, tpb  >>> (a, x, b, rows, cols);
+  multiply <<< bpg, tpb  >>> (a_g, x_g, b_g, rows, cols);
 
   assert(cudaSuccess == cudaMemcpy(b, b_g, rows*sizeof(double), cudaMemcpyDeviceToHost));
 
