1a23446f4a2ebab2a466c163247023419f2b5aa3
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2018-03-10 13:15:03 -0500

    Generating .dat files from metrics

--- print_metrics.sh
+++ print_metrics.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
 
-log=$1
+declare -r log=$1
+declare -r n=$(echo $log | tr -cd '[[:digit:]]')
 
-jacobi_vals=$(grep -R run_jacobi $log | awk '{print $5}')
-ceror_vals=$(grep -R compute_error $log | awk '{print $5}')
-cublas_vals=$(grep -R cublas $log | awk '{print $5}')
+function get_jacobi_metrics {
+    declare -r jacobi_vals=$(grep -R run_jacobi $log | awk '{print $5}')
+    declare -r ceror_vals=$(grep -R compute_error $log | awk '{print $5}')
+    declare -r cublas_vals=$(grep -R cublas $log | awk '{print $5}')
+    declare -r jacobi_err=$(grep -R jacobi_err $log | awk '{print $3}')
+    declare -r jacobi_iters=$(grep -R jacobi_iters $log | awk '{print $3}')
+    declare etime=0
 
-etime=0
-for val in $jacobi_vals; do
-    etime="$(echo "$etime + $val" | bc)"
-done
+    for val in $jacobi_vals; do
+	etime+=" + $val"
+    done
 
-echo "run_jacobi elapsed time: $etime ms"
+    for val in $ceror_vals; do
+	etime+=" + $val"
+    done
 
-etime=0
-for val in $ceror_vals; do
-    etime="$(echo "$etime + $val" | bc)"
-done
+    for val in $cublas_vals; do
+	etime+=" + $val"
+    done
 
-echo "compute_error elapsed time: $etime ms"
+    echo "$n $(echo $etime | bc)" >> jacobi_time.dat
+    echo "$n $jacobi_err" >> jacobi_err.dat
+    echo "$n $jacobi_iters" >> jacobi_iters.dat
+}
 
-etime=0
-for val in $cublas_vals; do
-    etime="$(echo "$etime + $val" | bc)"
-done
+function get_mkl_metrics {
+    declare -r mkl_err=$(grep -R mkl_err $log | awk '{print $3}')
+    declare -r mkl_iters=$(grep -R mkl_iters $log | awk '{print $3}')
+    declare etime="$(grep -R mkl_time $log | awk '{print $3}')*1000"
 
-echo "cublas elapsed time: $etime ms"
+    echo "$n $(echo $etime | bc)" >> mkl_time.dat
+    echo "$n $mkl_err" >> mkl_err.dat
+    echo "$n $mkl_iters" >> mkl_iters.dat
+}
 
-mkl_val=$(grep -R mkl_dgesv $log | awk '{print $5}')
-echo "mkl elapsed time: $mkl_val sec"
+function main {
+    declare -r jacobi_success=$(grep -R jacobi_success $log | awk '{print $3}')
+    declare -r mkl_success=$(grep -R mkl_success $log | awk '{print $3}')
+    if [ "$jacobi_success" == "no" ] || [ "$mkl_success" == "no" ]; then
+	exit 1
+    fi
+    get_jacobi_metrics
+    get_mkl_metrics
+}
+
+main
+exit 0
